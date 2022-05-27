bfd54bd39d0f6a6fb11d362a40d33b9cee86bf62
kasper108 <stanislovas.kasperiunas@gmail.com>
2022-05-27 12:14:35 +0300

    start

---
+++ src/main/java/tech/sapnas/lottyxbe/LottyxBeApplication.java
@@ -0,0 +1,13 @@
+package tech.sapnas.lottyxbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottyxBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottyxBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/sapnas/lottyxbe/LottyxBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sapnas.lottyxbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottyxBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
